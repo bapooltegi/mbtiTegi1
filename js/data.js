1931dabc564a1602fba2b4e8f28b5884c59a5d02
bapoolTegi <bapooltegi@gmail.com>
2022-06-28 23:20:35 +0900

    다음넘어가는 함수 -> 문제불러와서 -> 반복문으로 답변버튼 만듬.

---
+++ js/data.js
@@ -0,0 +1,38 @@
+const qnaList = [
+  //qnaList[0] 1번째 문항
+  {
+    q: "1. 이성 사이에 진정한 친구는 있다, 없다?",
+    a: [
+      { answer: "a. 이성 사이에 친구가 어딨어? 절대 없어",
+        type: ['cow','tiger','dragon','chick'] },
+      { answer: "b. 친구 있지, 절대 이성으로만 안보일 뿐",
+        type: ['mouse','rabbit','horse','snake','dog','monkey'] },
+      { answer: "c. 난 잘모르겠어..",
+        type: ['sheep','pig'] },
+    ]
+  },
+  //qnaList[1] 2번째 문항
+  {
+    q: "2. 좋아하는 사람이 생겨 연락하고자 한다",
+    a: [
+      { answer: "a. 바로 먼저 연락한다",
+        type:['mouse','rabbit','tiger','monkey'] },
+      { answer: "b. 언제 연락할지, 어떻게 말할지 심사숙고 하여 연락한다.",
+        type:['cow','horse','snake','dog'] },
+      { answer: "c. 상대방에게 먼저 연락 올 때까지 기다린다.",
+        type:['sheep','dragon','chick','pig']},
+    ]
+  },
+  //qnaList[2] 3번째 문항
+  {
+    q: "3. 3번째 문제다아아아",
+    a: [
+      { answer: "a. 3번째 문항11111",
+        type:['mouse','rabbit','tiger','monkey'] },
+      { answer: "b. 머라고오~~",
+        type:['cow','horse','snake','dog'] },
+      { answer: "c. 개쩐다!",
+        type:['sheep','dragon','chick','pig']},
+    ]
+  },
+]
--- js/start.js
+++ js/start.js
@@ -1,6 +1,35 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+
+//답변버튼 추가하는 함수
+function addAnswer(answerText){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button')
+  answer.classList.add('answerList'); //answer버튼에 answerList 클래스를 추가함.classList의 메소드 .add 클래스추가
+  a.appendChild(answer); //.answerBox에 answer 클래스 버튼들을 자식으로 넣음.
+  answer.innerHTML = answerText;
+
+  answer.addEventListener('click',function(){
+    var children = document.querySelectorAll('.answerList'); //answerList 클래스를 변수에 추가
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.display = 'none';
+    }
+  }, false);
+
+}
+//다음으로 넘어갈때 나오는 함수 -> 문제를 불러오고, 반복문으로 addAnswer()호출해서 버튼을 만듬.
+function goNext(qIdx){
+  var q = document.querySelector(".qbox");
+  q.innerHTML = qnaList[qIdx].q; //qnaList[0]번째에 있는 q를 불러와서 HTML텍스트를 붙힘.
+
+  for(let i in qnaList[qIdx].a){
+    // qnaList[qIdx]의 a 만큼 반복시킴.
+    addAnswer(qnaList[qIdx].a[i].answer);
+  }
+}
+//시작버튼 눌렀을때 실행하는 함수. goNext()로 문제를 만듬.
 function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s"
@@ -10,7 +39,9 @@ function begin() {
     setTimeout(()=>{
       main.style.display = "none";
       qna.style.display ="flex";
-    },450);
+    },450)
+    let qIdx = 0;
+    goNext(qIdx);
   },450);
 
 
